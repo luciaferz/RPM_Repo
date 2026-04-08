802f862fca060657eaef8da3ed4a6d3effccaf14
Alejandro Merino <rimerivas03@gmail.com>
2026-04-08 20:59:45 +0200

    Caja registro(codigo html,css y js)+enlace en el home

---
+++ Java_Script/registro.js
@@ -0,0 +1,60 @@
+let currentStep = 0;
+const steps = document.querySelectorAll(".step");
+const nextBtn = document.getElementById("nextBtn");
+const prevBtn = document.getElementById("prevBtn");
+const progressBar = document.getElementById("progressBar");
+
+showStep();
+
+function showStep() {
+  steps.forEach(s => s.classList.remove("active"));
+  steps[currentStep].classList.add("active");
+
+  prevBtn.style.display = currentStep === 0 ? "none" : "inline-block";
+
+  if (currentStep === steps.length - 1) {
+    nextBtn.textContent = "Finalizar";
+  } else {
+    nextBtn.textContent = "Siguiente";
+  }
+
+  progressBar.style.width = ((currentStep + 1) / steps.length) * 100 + "%";
+}
+
+nextBtn.addEventListener("click", () => {
+  if (!validar()) return;
+
+  if (currentStep < steps.length - 1) {
+    currentStep++;
+    showStep();
+  } else {
+    alert("Registro completado 🚀");
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  currentStep--;
+  showStep();
+});
+
+function validar() {
+  if (currentStep === 0) {
+    let email = document.getElementById("email").value;
+    let pass = document.getElementById("password").value;
+    let confirm = document.getElementById("confirmPassword").value;
+
+    if (!email || pass.length < 8 || pass !== confirm) {
+      alert("Revisa los datos");
+      return false;
+    }
+  }
+
+  if (currentStep === 3) {
+    if (!document.getElementById("terms").checked) {
+      alert("Acepta los términos");
+      return false;
+    }
+  }
+
+  return true;
+}
